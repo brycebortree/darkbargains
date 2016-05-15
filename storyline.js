6a66fea7e016649ef82e68acc70c105191b751b2
brytree <brycebortree@gmail.com>
2016-05-14 18:27:49 -0700

    small jQuery Work

---
+++ storyline.js
@@ -0,0 +1,24 @@
+
+var storyline = [
+  '<h1 class="fadein">dark bargains</h1>', 
+  '<p>You are thirty-one and you decide to live in her forest, because the children always come back.</p>', 
+  '<p> Not that you have children, with your husband who has shoulders like bear traps and who intimates about dark bargains that we all have to make when meeting strangers.</p>', 
+  '<p>You know dark bargains, you are one, and you help him build your house together at the base of her mountain, in the sweep of the forest that everyone has grown to avoid, on that mountain by the sea.</p>',  
+  '<p></p>',
+  '<p>It&apos;s good for the two of you because the bad men are kept away by the stories of her appearing with her netting that she doesn&apos;t use to catch fish. </p>', 
+  '<p>Your husband captures live animals and sells them to traveling circuses, but he doesn&apos;t declaw any of the big cats no matter how much the circuses ask him to</p>',  
+  '<p>The animals live on and on and sometimes they come back with scars and stories and sometimes they stalk around your home at night.</p>',
+  '<p class="asides"><strike><xsl:text>Sometimes they talk in the voices of those men you escaped.</xsl:text></strike></p>',
+  '<p>But they never come through the door.</p>',
+  '<p></p>',
+  '<p>She never comes to your door, even though for that first year you were ready.</p>',
+  '<p>Every month you were glad to have the blood pooling out of you and pushing through the marriage bed you&apos;re making, into the water in the bath your husband had chipped for you. </p>',
+  '<p>You are expecting her to show up with her long neck and say, own up.</p>',
+  '<p>But instead, you spend your days in a garden that wends down the hill and you gather enough food for you and your husband.</p>',
+  '<p>He comes home and you&apos;ve dug out of the walls of your home for more space to store up food. </p>',
+  // FOOD appears in the corner, increasing with each page
+  '<p>Winter comes and your husband&apos;s away when that blizzard hits so he has to stay away.</p>',
+  '<p>You have enough food to last you and him and more and you are thirty-four and something in you says, it is time.</p>',
+  '<p class="asides"><strike><xsl:text>You want something that doesn&apos;t travel away.</xsl:text></strike></p>',
+  '<p></p>',
+  '<p></p>'];
