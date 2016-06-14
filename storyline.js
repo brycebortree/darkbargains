986096f5235382338faabc450b6288e2aabdb446
brytree <brycebortree@gmail.com>
2016-06-13 17:48:34 -0700

    adding Todd strategy

--- storyline.js
+++ storyline.js
@@ -3,7 +3,7 @@ var storyline = [
   '<h1 class="fadein">dark bargains</h1>', 
   '<p>You are thirty-one and you decide to live in her forest, because the children always come back.</p>', 
   '<p> Not that you have children, with your husband who has shoulders like bear traps and who intimates about dark bargains that we all have to make when meeting strangers.</p>', 
-  '<p>You know dark bargains, you are one, and you help him build your house together at the base of her mountain, in the sweep of the forest that everyone has grown to avoid, on that mountain by the sea.</p>',  
+  '<p class="sentence3">You know dark bargains, you are one, and you help him build your house together at the base of her mountain, in the sweep of the forest that everyone has grown to avoid, on that mountain by the sea.</p>',  
   '<p></p>',
   '<p>It&apos;s good for the two of you because the bad men are kept away by the stories of her appearing with her netting that she doesn&apos;t use to catch fish. </p>', 
   '<p>Your husband captures live animals and sells them to traveling circuses, but he doesn&apos;t declaw any of the big cats no matter how much the circuses ask him to</p>',  
@@ -22,3 +22,5 @@ var storyline = [
   '<p class="asides"><strike><xsl:text>You want something that doesn&apos;t travel away.</xsl:text></strike></p>',
   '<p></p>',
   '<p></p>'];
+
+  var sentence3 = []
